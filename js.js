b5f1f0a7b1d585f80de57afda39e6f8f85f49389
Chris Loewer <chris.loewer@gmail.com>
2015-09-26 23:49:11 -0500

    Sidebars initialized.

--- js.js
+++ js.js
@@ -2,3 +2,10 @@ var mainDiv = document.getElementById('main');
 
 window.location.hash = 'main';
 
+// page tail
+window.onload = function() {
+	
+	/* Disable middle mouse button */
+	document.body.onmousedown = function(e) { if (e.button === 1) return false; }
+}
+
