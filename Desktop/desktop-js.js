09cb40353379582973e0de0f2321e336ab15314a
Chris Loewer <chris.loewer@gmail.com>
2015-10-05 17:46:49 -0500

    Clean up animations and page select.

--- Desktop/desktop-js.js
+++ Desktop/desktop-js.js
@@ -1,18 +1,15 @@
 var body;
-var pageNumber;
+var destPageNumber;
+var activePageNumber;
 
 window.onload = function() {
 	body = document.getElementById('body');
 
-	pageNumber = 1;
-	// pgOne_Load();
-
-	pgOne();
+	destPageNumber = 1;
+	loadPage();
 }
 
 function reset() {
-	console.log('reset');
-
 	var left_divs = document.getElementsByClassName('left');
 	var right_divs = document.getElementsByClassName('right');
 
@@ -20,68 +17,43 @@ function reset() {
 	for(i=0; i<left_divs.length; i++) {
 		var element = left_divs[i];
 		element.className = 'left';
-		element.dataset.active = '0';
-		// removeAllEvents(element, 'transitionend');
-
 		element.removeEventListener('transitionend', reset);
-		// element.removeEventListener('transitionend', pgOne);
-		// element.removeEventListener('transitionend', pgTwo);
-		// element.removeEventListener('transitionend', reset);
 	}
-
 	for(i=0; i<right_divs.length; i++) {
 		right_divs[i].className = 'right';
 	}
+
+	loadPage();
 }
 
 function fadeOut() {
-	var activeElement = document.getElementById('left-' + pageNumber);
-	var partnerElement = document.getElementById('right-' + pageNumber);
+	var activeElement = document.getElementById('left-' + activePageNumber);
+	var partnerElement = document.getElementById('right-' + activePageNumber);
+
 	activeElement.className += ' anim-out';
 	partnerElement.className += ' anim-out';
-	body.className = 'body page-' + pageNumber;
+	body.className = 'body page-' + activePageNumber;
 
 	activeElement.addEventListener('transitionend', reset);
 }
 
-function pgOne_Load() {
-	// reset();
-	fadeOut();
-	// pgOne();
+// CLICK HANDLERS
+function pgOne_Click() {
+	if(activePageNumber != 1) {
+		destPageNumber = 1;
+		fadeOut();
+	}
 }
-function pgOne() {
-	console.log('pgOne');
-
-	var element = document.getElementById('left-1');
-
-	element.className += ' anim';
-	document.getElementById('right-1').className += ' anim';
-	element.dataset.active = '1';
-
-	pageNumber = 1;
-
-	// removeAllEvents(element, 'transitionend');
+function pgTwo_Click() {
+	if(activePageNumber != 2){
+		destPageNumber = 2;
+		fadeOut();
+	}
 }
 
-function pgTwo_Load() {
-	// reset();
-	fadeOut(pgTwo);
-	// pgTwo();
-}
-function pgTwo() {
-	console.log('pgTwo');
-
-	var element = document.getElementById('left-2');
-
-	document.getElementById('right-2').className += ' anim';
-	element.className += ' anim';
-	element.dataset.active = '1';
-
-	pageNumber = 2;
-
-	// removeAllEvents(element, 'transitionend');
-}
-
-function msg() {
-	console.log('MSG funct');
+// LOAD PAGE
+function loadPage() {
+	document.getElementById('left-' + destPageNumber).className += ' anim';
+	document.getElementById('right-' + destPageNumber).className += ' anim';
+	activePageNumber = destPageNumber;
 }
