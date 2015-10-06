8425f9c06364c7c983e41e5e6a0cc85edbfc2892
Chris Loewer <chris.loewer@gmail.com>
2015-10-06 17:32:21 -0500

    Change tabs to 2 spaces.

--- Desktop/desktop-js.js
+++ Desktop/desktop-js.js
@@ -5,86 +5,86 @@ var buttons;
 var NUM_PAGES = 4;
 
 window.onload = function() {
-	body = document.getElementById('body');
-	body.addEventListener('wheel', mouseWheelController);
-	buttons = document.getElementsByClassName('pgButton');
+  body = document.getElementById('body');
+  body.addEventListener('wheel', mouseWheelController);
+  buttons = document.getElementsByClassName('pgButton');
 
-	destPageNumber = 1;
-	loadPage();
+  destPageNumber = 1;
+  loadPage();
 }
 
 function reset() {
-	var left_divs = document.getElementsByClassName('left');
-	var right_divs = document.getElementsByClassName('right');
+  var left_divs = document.getElementsByClassName('left');
+  var right_divs = document.getElementsByClassName('right');
 
-	var i;
-	for(i=0; i<left_divs.length; i++) {
-		var element = left_divs[i];
-		element.className = 'left';
-		element.removeEventListener('transitionend', reset);
-	}
-	for(i=0; i<right_divs.length; i++) {
-		right_divs[i].className = 'right';
-	}
+  var i;
+  for(i=0; i<left_divs.length; i++) {
+    var element = left_divs[i];
+    element.className = 'left';
+    element.removeEventListener('transitionend', reset);
+  }
+  for(i=0; i<right_divs.length; i++) {
+    right_divs[i].className = 'right';
+  }
 
-	loadPage();
+  loadPage();
 }
 
 function updatePageButtons() {
-	var i;
-	for(i=0; i<buttons.length; i++) {
-		buttons[i].className = 'pgButton';
-	}
-	buttons[activePageNumber-1].className += ' active';
+  var i;
+  for(i=0; i<buttons.length; i++) {
+    buttons[i].className = 'pgButton';
+  }
+  buttons[activePageNumber-1].className += ' active';
 }
 
 function fadeOut() {
-	var activeElement = document.getElementById('left-' + activePageNumber);
-	var partnerElement = document.getElementById('right-' + activePageNumber);
+  var activeElement = document.getElementById('left-' + activePageNumber);
+  var partnerElement = document.getElementById('right-' + activePageNumber);
 
-	activeElement.className += ' anim-out';
-	partnerElement.className += ' anim-out';
-	body.className = 'body page-' + activePageNumber;
+  activeElement.className += ' anim-out';
+  partnerElement.className += ' anim-out';
+  body.className = 'body page-' + activePageNumber;
 
-	activeElement.addEventListener('transitionend', reset);
+  activeElement.addEventListener('transitionend', reset);
 }
 
 // CLICK HANDLERS
 function pg_click(n) {
-	if(activePageNumber != n) {
-		destPageNumber = n;
-		fadeOut();
-	}
+  if(activePageNumber != n) {
+    destPageNumber = n;
+    fadeOut();
+  }
 }
 
 function mouseWheelController(e) {
-	if(e.deltaY > 0) {
-		nextPage();
-	}
-	else if(e.deltaY < 0) {
-		prevPage();
-	}
+  if(e.deltaY > 0) {
+    nextPage();
+  }
+  else if(e.deltaY < 0) {
+    prevPage();
+  }
 }
 
 // LOAD PAGE
 function loadPage() {
-	document.getElementById('left-' + destPageNumber).className += ' anim';
-	document.getElementById('right-' + destPageNumber).className += ' anim';
-	activePageNumber = destPageNumber;
-	updatePageButtons()
+  document.getElementById('left-' + destPageNumber).className += ' anim';
+  document.getElementById('right-' + destPageNumber).className += ' anim';
+  activePageNumber = destPageNumber;
+  updatePageButtons()
 }
 
 function nextPage() {
-	if(activePageNumber < NUM_PAGES && destPageNumber < NUM_PAGES) {
-		destPageNumber++;
-		fadeOut();
-	}
+  if(activePageNumber < NUM_PAGES && destPageNumber < NUM_PAGES) {
+    destPageNumber++;
+    fadeOut();
+  }
 }
 
 function prevPage() {
-	if(activePageNumber > 1 && destPageNumber > 1) {
-		destPageNumber--;
-		fadeOut();
-	}
+  if(activePageNumber > 1 && destPageNumber > 1) {
+    destPageNumber--;
+    fadeOut();
+  }
 }
 
